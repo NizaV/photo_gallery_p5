4c631dfa483bdb553c7c09f24fccd13dffb9ec01
NizaV <60785417+NizaV@users.noreply.github.com>
2020-05-04 18:26:51 -0400

    Attempting to fix the search function.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
+//lightbox javascript
+
 lightbox.option({
     'resizeDuration': 200,
     'alwaysShowNavOnTouchDevices': true,
     'wrapAround': true
-  });   
-      
+  });
+
+// javascript for search input 
+
+const $lightboxPhoto = $('.row a');
+$(".lightbox-image").hide().delay(200).fadeIn(300);
+
+
+$('#search').on('keyup', function() {
+    let $search = $('#search').val().toLowercase();
+    for (let i = 0; i < $lightboxPhoto.length; i += 1) {
+        let $inputPhoto = $lightboxPhoto.eq(i);
+        if ($inputPhoto.attr("data-title").toLowercase().indexOf($search) === -1) {
+            $inputPhoto.fadeOut(200);
+        }
+        if ($inputPhoto.attr("data-title").toLowercase().indexOf($search) !== -1) {
+            $inputPhoto.hide().fadeIn(200);
+        }
+    }
+    
+});
+
     
